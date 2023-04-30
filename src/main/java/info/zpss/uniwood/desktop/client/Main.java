54881ce8dcaeb6d6fe797b448572e1f98deba539
Zpss <2125060196@henu.edu.cn>
2023-05-01 05:39:54 +0800

    修改参数说明文件格式 日志系统微调 添加日志目录到仓库

--- src/main/java/info/zpss/uniwood/desktop/client/Main.java
+++ src/main/java/info/zpss/uniwood/desktop/client/Main.java
@@ -3,14 +3,24 @@ package info.zpss.uniwood.desktop.client;
 import info.zpss.uniwood.desktop.client.tools.*;
 
 public class Main {
-    private static boolean DEBUG = true;  // TODO: 打包jar前改为false
-    public static final String PLATFORM = "DESKTOP";
-    public static final String VERSION = "1.0.0";
+    private static boolean DEBUG;
+    private static String[] arguments;
+    public static final String PLATFORM;
+    public static final String VERSION;
+
+    static {
+        DEBUG = true;   // TODO: 打包jar前改为false
+        arguments = null;
+        PLATFORM = "DESKTOP";
+        VERSION = "1.0.0";
+    }
 
     public static void main(String[] args) {
-        if (paramInArgs("-D", args) || paramInArgs("--DEBUG", args))
+        arguments = new String[args.length];
+        System.arraycopy(args, 0, arguments, 0, args.length);
+        if (paramInArgs("-D") || paramInArgs("--DEBUG"))
             DEBUG = true;
-        setLog(args);
+        setLog();
         System.out.println("Hello, UniWood!");
     }
 
@@ -18,26 +28,26 @@ public class Main {
         return Main.DEBUG;
     }
 
-    private static boolean paramInArgs(String para, String[] args) {
-        for (String arg : args)
+    public static boolean paramInArgs(String para) {
+        for (String arg : arguments)
             if (arg.equals(para))
                 return true;
         return false;
     }
 
-    private static String stringInArgs(String para, String[] args) {
-        for (int i = 0; i < args.length; i++)
-            if (args[i].equals(para))
-                if (i + 1 < args.length)
-                    return args[i + 1];
+    public static String stringInArgs(String para) {
+        for (int i = 0; i < arguments.length; i++)
+            if (arguments[i].equals(para))
+                if (i + 1 < arguments.length)
+                    return arguments[i + 1];
         return null;
     }
 
-    private static void setLog(String[] args) {
+    private static void setLog() {
         String logDir = DEBUG ? "src/main/logs" : "logs";
-        String fromArgs = stringInArgs("-l", args);
+        String fromArgs = stringInArgs("-l");
         Log.setLogFileDir((((fromArgs == null) ?
-                (fromArgs = stringInArgs("--len", args)) : fromArgs) == null) ?
+                (fromArgs = stringInArgs("--len")) : fromArgs) == null) ?
                 logDir : fromArgs);
         Log.add(String.format("UniWood-%s-%s", PLATFORM, VERSION), Log.Type.INFO, Thread.currentThread());
         if (DEBUG)
