6428957f6838691bd64bd2f0f6466dc06e20c1bc
Zpss <2125060196@henu.edu.cn>
2023-05-10 17:24:05 +0800

    桌面客户端Mapper层接口初步实现

---
+++ src/main/java/info/zpss/uniwood/desktop/server/mapper/Impl/PostMapperImpl.java
@@ -0,0 +1,144 @@
+package info.zpss.uniwood.desktop.server.mapper.Impl;
+
+import info.zpss.uniwood.desktop.common.Log;
+import info.zpss.uniwood.desktop.server.Main;
+import info.zpss.uniwood.desktop.server.mapper.PostMapper;
+import info.zpss.uniwood.desktop.server.model.Post;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostMapperImpl implements PostMapper {
+    private static final PostMapper INSTANCE = new PostMapperImpl();
+
+    private PostMapperImpl() {
+    }
+
+    public static PostMapper getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public synchronized Post getPost(Integer postID) {
+        Post post = new Post();
+        String sql = "SELECT * FROM tb_post WHERE id = ?";
+        try (Connection conn = Main.database().getConnection()) {
+            PreparedStatement preStmt = conn.prepareStatement(sql);
+            preStmt.setInt(1, postID);
+            ResultSet resultSet = preStmt.executeQuery();
+            if (resultSet.next()) {
+                post.setId(resultSet.getInt("id"));
+                post.setZoneId(resultSet.getInt("zone_id"));
+            }
+            resultSet.close();
+            preStmt.close();
+        } catch (SQLException e) {
+            Log.add(e, Thread.currentThread());
+        }
+        return post;
+    }
+
+    @Override
+    public synchronized List<Post> getPosts() {
+        List<Post> posts = new ArrayList<>();
+        String sql = "SELECT * FROM tb_post";
+        try (Connection conn = Main.database().getConnection()) {
+            PreparedStatement preStmt = conn.prepareStatement(sql);
+            ResultSet resultSet = preStmt.executeQuery();
+            while (resultSet.next()) {
+                Post post = new Post();
+                post.setId(resultSet.getInt("id"));
+                post.setZoneId(resultSet.getInt("zone_id"));
+                posts.add(post);
+            }
+            resultSet.close();
+            preStmt.close();
+        } catch (SQLException e) {
+            Log.add(e, Thread.currentThread());
+        }
+        return posts;
+    }
+
+    @Override
+    public synchronized List<Post> getPostsByZoneID(Integer zoneID) {
+        List<Post> posts = new ArrayList<>();
+        String sql = "SELECT * FROM tb_post WHERE zone_id = ?";
+        try (Connection conn = Main.database().getConnection()) {
+            PreparedStatement preStmt = conn.prepareStatement(sql);
+            preStmt.setInt(1, zoneID);
+            ResultSet resultSet = preStmt.executeQuery();
+            while (resultSet.next()) {
+                Post post = new Post();
+                post.setId(resultSet.getInt("id"));
+                post.setZoneId(resultSet.getInt("zone_id"));
+                posts.add(post);
+            }
+            resultSet.close();
+            preStmt.close();
+        } catch (SQLException e) {
+            Log.add(e, Thread.currentThread());
+        }
+        return posts;
+    }
+
+    @Override
+    public synchronized List<Post> getPostsByUserID(Integer userID) {
+        List<Post> posts = new ArrayList<>();
+        String sql = "SELECT * FROM tb_post WHERE id IN (SELECT post_id FROM tb_user_post WHERE user_id = ?)";
+        try (Connection conn = Main.database().getConnection()) {
+            PreparedStatement preStmt = conn.prepareStatement(sql);
+            preStmt.setInt(1, userID);
+            ResultSet resultSet = preStmt.executeQuery();
+            while (resultSet.next()) {
+                Post post = new Post();
+                post.setId(resultSet.getInt("id"));
+                post.setZoneId(resultSet.getInt("zone_id"));
+                posts.add(post);
+            }
+            resultSet.close();
+            preStmt.close();
+        } catch (SQLException e) {
+            Log.add(e, Thread.currentThread());
+        }
+        return posts;
+    }
+
+    @Override
+    public synchronized Integer addPost(Integer zoneID) {
+        String sql_add = "INSERT INTO tb_post (zone_id) VALUES (?)";
+        String sql_get = "SELECT MAX(id) FROM tb_post";
+        Integer id = null;
+        try (Connection conn = Main.database().getConnection()) {
+            PreparedStatement preStmtAdd = conn.prepareStatement(sql_add);
+            preStmtAdd.setInt(1, zoneID);
+            preStmtAdd.executeUpdate();
+            preStmtAdd.close();
+            PreparedStatement preStmtGet = conn.prepareStatement(sql_get);
+            ResultSet resultSet = preStmtGet.executeQuery();
+            if (resultSet.next())
+                id = resultSet.getInt(1);
+            resultSet.close();
+            preStmtGet.close();
+        } catch (SQLException e) {
+            Log.add(e, Thread.currentThread());
+        }
+        return id;
+    }
+
+    @Override
+    public synchronized void deletePost(Integer postID) {
+        String sql = "DELETE FROM tb_post WHERE id = ?";
+        try (Connection conn = Main.database().getConnection()) {
+            PreparedStatement preStmt = conn.prepareStatement(sql);
+            preStmt.setInt(1, postID);
+            preStmt.executeUpdate();
+            preStmt.close();
+        } catch (SQLException e) {
+            Log.add(e, Thread.currentThread());
+        }
+    }
+}
--- src/main/java/info/zpss/uniwood/desktop/server/mapper/Impl/UserMapperImpl.java
+++ src/main/java/info/zpss/uniwood/desktop/server/mapper/Impl/UserMapperImpl.java
@@ -3,23 +3,28 @@ package info.zpss.uniwood.desktop.server.mapper.Impl;
 import info.zpss.uniwood.desktop.common.Log;
 import info.zpss.uniwood.desktop.server.Main;
 import info.zpss.uniwood.desktop.server.mapper.UserMapper;
-import info.zpss.uniwood.desktop.server.model.Post;
 import info.zpss.uniwood.desktop.server.model.User;
-import info.zpss.uniwood.desktop.server.model.Zone;
+import info.zpss.uniwood.desktop.server.model.UserZone;
+import info.zpss.uniwood.desktop.server.model.UserPost;
+import info.zpss.uniwood.desktop.server.model.Follow;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserMapperImpl implements UserMapper {
-    public UserMapperImpl() {
+    private static final UserMapper INSTANCE = new UserMapperImpl();
+
+    private UserMapperImpl() {
+    }
+
+    public static UserMapper getInstance() {
+        return INSTANCE;
     }
 
     @Override
-    public User getUser(Integer userID) {
+    public synchronized User getUser(Integer userID) {
         User user = new User();
         String sql = "SELECT * FROM tb_user WHERE id = ?";
         try (Connection conn = Main.database().getConnection()) {
@@ -43,7 +48,7 @@ public class UserMapperImpl implements UserMapper {
     }
 
     @Override
-    public List<User> getUsers() {
+    public synchronized List<User> getUsers() {
         List<User> users = new ArrayList<>();
         String sql = "SELECT * FROM tb_user";
         try (Connection conn = Main.database().getConnection()) {
@@ -68,15 +73,41 @@ public class UserMapperImpl implements UserMapper {
     }
 
     @Override
-    public List<User> getUserFollowers(Integer userID) {
-        List<User> followers = new ArrayList<>();
-        String sql = "SELECT follower FROM tb_follow WHERE following = ?";
+    public synchronized String getStatus(Integer userID) {
+        String sql = "SELECT status FROM tb_user WHERE id = ?";
         try (Connection conn = Main.database().getConnection()) {
             PreparedStatement preStmt = conn.prepareStatement(sql);
             preStmt.setInt(1, userID);
             ResultSet resultSet = preStmt.executeQuery();
-            while (resultSet.next())
-                followers.add(getUser(resultSet.getInt("follower")));
+            if (resultSet.next()) {
+                return resultSet.getString("status");
+            }
+            resultSet.close();
+            preStmt.close();
+        } catch (SQLException e) {
+            Log.add(e, Thread.currentThread());
+        }
+        return null;
+    }
+
+    @Override
+    public synchronized List<User> getFollowers(Integer userID) {
+        List<User> followers = new ArrayList<>();
+        String sql = "SELECT * FROM tb_user WHERE id IN (SELECT follower FROM tb_follow WHERE following = ?)";
+        try (Connection conn = Main.database().getConnection()) {
+            PreparedStatement preStmt = conn.prepareStatement(sql);
+            preStmt.setInt(1, userID);
+            ResultSet resultSet = preStmt.executeQuery();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUsername(resultSet.getString("username"));
+                user.setPassword(resultSet.getString("password"));
+                user.setAvatar(resultSet.getString("avatar"));
+                user.setUniversity(resultSet.getString("university"));
+                user.setStatus(resultSet.getString("status"));
+                followers.add(user);
+            }
             resultSet.close();
             preStmt.close();
         } catch (SQLException e) {
@@ -86,15 +117,23 @@ public class UserMapperImpl implements UserMapper {
     }
 
     @Override
-    public List<User> getUserFollowings(Integer userID) {
+    public synchronized List<User> getFollowings(Integer userID) {
         List<User> followings = new ArrayList<>();
-        String sql = "SELECT following FROM tb_follow WHERE follower = ?";
+        String sql = "SELECT * FROM tb_user WHERE id IN (SELECT following FROM tb_follow WHERE follower = ?)";
         try (Connection conn = Main.database().getConnection()) {
             PreparedStatement preStmt = conn.prepareStatement(sql);
             preStmt.setInt(1, userID);
             ResultSet resultSet = preStmt.executeQuery();
-            while (resultSet.next())
-                followings.add(getUser(resultSet.getInt("following")));
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUsername(resultSet.getString("username"));
+                user.setPassword(resultSet.getString("password"));
+                user.setAvatar(resultSet.getString("avatar"));
+                user.setUniversity(resultSet.getString("university"));
+                user.setStatus(resultSet.getString("status"));
+                followings.add(user);
+            }
             resultSet.close();
             preStmt.close();
         } catch (SQLException e) {
@@ -104,53 +143,93 @@ public class UserMapperImpl implements UserMapper {
     }
 
     @Override
-    public List<Zone> getUserZones(Integer userID) {
-        List<Zone> zones = new ArrayList<>();
-        String sql = "SELECT * FROM tb_zone WHERE id IN " +
-                "(SELECT zone_id FROM tb_user_zone WHERE user_id = ?)"; // TODO
+    public synchronized void addUser(User user) {
+        String sql = "INSERT INTO tb_user(username, password, avatar, university, status) VALUES (?, ?, ?, ?, ?)";
         try (Connection conn = Main.database().getConnection()) {
             PreparedStatement preStmt = conn.prepareStatement(sql);
-            preStmt.setInt(1, userID);
-            ResultSet resultSet = preStmt.executeQuery();
-            while (resultSet.next()) {
-                Zone zone = new Zone();
-                zone.setId(resultSet.getInt("id"));
-                zone.setName(resultSet.getString("name"));
-                zone.setIcon(resultSet.getString("icon"));
-                zone.setDescription(resultSet.getString("description"));
-                zones.add(zone);
-            }
-            resultSet.close();
+            preStmt.setString(1, user.getUsername());
+            preStmt.setString(2, user.getPassword());
+            preStmt.setString(3, user.getAvatar());
+            preStmt.setString(4, user.getUniversity());
+            preStmt.setString(5, user.getStatus());
+            preStmt.executeUpdate();
             preStmt.close();
         } catch (SQLException e) {
             Log.add(e, Thread.currentThread());
         }
-        return zones;
     }
 
     @Override
-    public List<Post> getUserPosts(Integer userID) {
-        List<Post> posts = new ArrayList<>();
-        String sql = "SELECT * FROM tb_post WHERE id IN " +
-                "(SELECT post_id FROM tb_user_post WHERE user_id = ?)"; // TODO
+    public synchronized void addUserZone(UserZone userZone) {
+        String sql = "INSERT INTO tb_user_zone(user_id, zone_id) VALUES (?, ?)";
         try (Connection conn = Main.database().getConnection()) {
             PreparedStatement preStmt = conn.prepareStatement(sql);
-            preStmt.setInt(1, userID);
-            ResultSet resultSet = preStmt.executeQuery();
-            while (resultSet.next()) {
-                Post post = new Post();
-                post.setId(resultSet.getInt("id"));
-//                post.setZone(resultSet.getInt("zone_id"));
-                post.setAuthor(getUser(userID));
-//                post.setFloors(resultSet.getString("content"));
-//                post.setTime(resultSet.getTimestamp("create_time"));  TODO
-                posts.add(post);
-            }
-            resultSet.close();
+            preStmt.setInt(1, userZone.getUserId());
+            preStmt.setInt(2, userZone.getZoneId());
+            preStmt.executeUpdate();
             preStmt.close();
         } catch (SQLException e) {
             Log.add(e, Thread.currentThread());
         }
-        return posts;
+    }
+
+    @Override
+    public synchronized void addUserPost(UserPost userPost) {
+        String sql = "INSERT INTO tb_user_post(user_id, post_id) VALUES (?, ?)";
+        try (Connection conn = Main.database().getConnection()) {
+            PreparedStatement preStmt = conn.prepareStatement(sql);
+            preStmt.setInt(1, userPost.getUserId());
+            preStmt.setInt(2, userPost.getPostId());
+            preStmt.executeUpdate();
+            preStmt.close();
+        } catch (SQLException e) {
+            Log.add(e, Thread.currentThread());
+        }
+    }
+
+    @Override
+    public synchronized void addFollow(Follow follow) {
+        String sql = "INSERT INTO tb_follow(follower, following) VALUES (?, ?)";
+        try (Connection conn = Main.database().getConnection()) {
+            PreparedStatement preStmt = conn.prepareStatement(sql);
+            preStmt.setInt(1, follow.getFollowerId());
+            preStmt.setInt(2, follow.getFollowingId());
+            preStmt.executeUpdate();
+            preStmt.close();
+        } catch (SQLException e) {
+            Log.add(e, Thread.currentThread());
+        }
+    }
+
+    @Override
+    public synchronized void updateUser(User user) {
+        String sql = "UPDATE tb_user SET username = ?, password = ?, avatar = ?, university = ?, status = ? WHERE id = ?";
+        try (Connection conn = Main.database().getConnection()) {
+            PreparedStatement preStmt = conn.prepareStatement(sql);
+            preStmt.setString(1, user.getUsername());
+            preStmt.setString(2, user.getPassword());
+            preStmt.setString(3, user.getAvatar());
+            preStmt.setString(4, user.getUniversity());
+            preStmt.setString(5, user.getStatus());
+            preStmt.setInt(6, user.getId());
+            preStmt.executeUpdate();
+            preStmt.close();
+        } catch (SQLException e) {
+            Log.add(e, Thread.currentThread());
+        }
+    }
+
+    @Override
+    public synchronized void updateStatus(Integer userID, String status) {
+        String sql = "UPDATE tb_user SET status = ? WHERE id = ?";
+        try (Connection conn = Main.database().getConnection()) {
+            PreparedStatement preStmt = conn.prepareStatement(sql);
+            preStmt.setString(1, status);
+            preStmt.setInt(2, userID);
+            preStmt.executeUpdate();
+            preStmt.close();
+        } catch (SQLException e) {
+            Log.add(e, Thread.currentThread());
+        }
     }
 }
